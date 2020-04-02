3e531baabb670734515edd7eddfde431395fa233
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-02 22:53:06 +0530

    update test case file

--- MIT_Assignment/test.cpp
+++ MIT_Assignment/test.cpp
@@ -1,11 +1,12 @@
 // This is the test code.
 #include <iostream>
-
+#define o 1;
 using namespace std;
 
 int main()
 {
-    while(1){
-        cout<<"sarvesh";
+    char s[] = "Hello,World!";
+    for(int i =0 ; i < sizeof(s);i++){
+        cout << s[i] << " ";
     }
 }
