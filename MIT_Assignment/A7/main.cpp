7a92f6b31da9df89e0a49ad41a6bee5f1f1bd79c
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-07 00:29:50 +0530

    complete a7

---
+++ MIT_Assignment/A7/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int length(char *str)
+{
+    int size = 0;
+    for (int i = 0; i < str[i] != '\0'; i++)
+    {
+        size++;
+    }
+    return size;
+}
+void swapByPointer(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void swapByRef(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void swap(int **ptr1, int **ptr2)
+{
+    int *temp;
+    temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
+int main()
+{
+    char str[] = "Sarvesh";
+    cout << length(str) << endl;
+    int a = 10, b = 20;
+    int c = 30, d = 40;
+    swapByPointer(&a, &b);
+    swapByRef(c, d);
+    cout << a << b << endl;
+    cout << c << d << endl;
+    int x = 5, y = 6;
+    int *ptr1 = &x, *ptr2 = &y;
+    swap(&ptr1, &ptr2);
+    cout << *ptr1 << *ptr2 << endl;
+}
