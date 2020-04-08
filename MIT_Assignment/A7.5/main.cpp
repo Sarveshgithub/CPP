580d832dcfb7310da34c0c9dba2f6645b9f839d1
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-08 23:09:07 +0530

    complete pointers

--- MIT_Assignment/A7.5/main.cpp
+++ MIT_Assignment/A7.5/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-#include<stdio.h>
+#include <stdio.h>
 using namespace std;
-int main(){
+int main()
+{
     char oddOrEven[] = "Never odd or even";
-    cout<<oddOrEven;
+    char *nthCharPtr = &(oddOrEven[5]);
+    oddOrEven[4] = *nthCharPtr;
+    char **pointerPtr = &nthCharPtr;
+
+    cout << oddOrEven << endl;
+    cout << *nthCharPtr;
 }
