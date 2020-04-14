79d8129e7652af66d9bf5a9ab7d99f5195391103
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-14 23:00:00 +0530

    Done prob 2

---
+++ CP/Beautiful_Matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            int x, y = 0, input, ans;
+            cin >> input;
+            y = (i > 1 && i < 5) ? i == 2 ? 1 : i == 3 ? 2 : i == 4 ? 1 : y : y;
+            x = j == 4 ? 2 : j == 5 ? 1 : j;
+            ans = 5 - x - y;
+            if (input == 1)
+            {
+                a = ans;
+                break;
+            }
+        }
+    }
+    cout << a;
+}
