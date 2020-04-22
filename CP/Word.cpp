30b4e7511fae70a59ab394ee981da36f9cf41f6c
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-22 16:20:53 +0530

    Add data

---
+++ CP/Word.cpp
@@ -0,0 +1,12 @@
+//http://codeforces.com/problemset/problem/59/A
+#include<iostream>
+#include <string>
+using namespace std;
+int main(){
+    bool isUpper;
+    string str;
+    getline(cin,str);
+    for(int i =0 ; i <= str.length(); i++){
+        str[i];
+    }
+}
