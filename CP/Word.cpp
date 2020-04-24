76aea9ffc8fe2927e9ee55cbe274cf6b5e40286a
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-24 20:36:11 +0530

    word

--- CP/Word.cpp
+++ CP/Word.cpp
@@ -1,12 +1,33 @@
 //http://codeforces.com/problemset/problem/59/A
-#include<iostream>
+#include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     bool isUpper;
+    int s = 0, l = 0;
     string str;
-    getline(cin,str);
-    for(int i =0 ; i <= str.length(); i++){
-        str[i];
+    getline(cin, str);
+    for (int i = 0; i <= str.length(); i++)
+    {
+        if (isupper(str[i]))
+        {
+            ++l;
+        }
+        else if (islower(str[i]))
+        {
+            ++s;
+        }
     }
+    if (l == s || s > l)
+    {
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+    }
+    else if (l > s)
+    {
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+    }
+
+    cout << str;
 }
