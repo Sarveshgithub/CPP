9a76862b4620aae9dd741dc9c20e4102fd5e93d6
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-24 23:20:54 +0530

    Word_capital

---
+++ CP/Word_Capitalization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    if (islower(s[0]))
+    {
+        s[0] = toupper(s[0]);
+    }
+    cout << s;
+}
