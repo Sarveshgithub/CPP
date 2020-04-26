cc0bf156c893b7ba369723da7dbd9a5d11d82168
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-26 18:44:26 +0530

    Added panaramix problem

---
+++ CP/Panoramix's Prediction.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/80/A
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+int main(){
+    f(i,5){
+        cout<<i;
+    }
+}
---
+++ template.cpp
@@ -0,0 +1,7 @@
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+int main(){
+}
