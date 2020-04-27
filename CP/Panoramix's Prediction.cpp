d129cfa52436406f444399797a6c333ea4418552
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-04-27 23:32:22 +0530

    incomplete panoramix

--- CP/Panoramix's Prediction.cpp
+++ CP/Panoramix's Prediction.cpp
@@ -1,11 +1,34 @@
-// https://codeforces.com/problemset/problem/80/A
+//https://codeforces.com/problemset/problem/80/A
 //Author : Sarvesh
 #include <bits/stdc++.h>
 using namespace std;
 #define f(x, n) for (int x = 0; x < n; ++x)
 #define f1(x, n) for (int x = 1; x <= n; ++x)
-int main(){
-    f(i,5){
-        cout<<i;
+#define f2(x, n) for (int x = 2; x <= n; ++x)
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    n++;
+    bool isPrime;
+    bool isYes = false;
+    while (n <= p)
+    {
+        isPrime = true;
+        for (int i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime == true)
+        {
+            isYes = true;
+            break;
+        }
+        n++;
     }
+    cout << (isYes == false ? "NO" : "YES");
 }
