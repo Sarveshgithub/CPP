b55081edca97efcd48e2a5b33931c41f3b6b11ab
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-01 23:58:03 +0530

    p1 hackerrank

---
+++ C++ HackerRank/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,18 @@
+//prob - https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+int main() {
+    int a; long b; char c; float d; double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout.precision(3);
+    cout<<fixed<<d<<endl;
+    cout.precision(9);
+    cout<<fixed<<e<<endl;
+    return 0;
+}
