898c87bd12f8f4abff759e7e00e3c167e1bc5564
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-07 00:00:07 +0530

    sdfds

---
+++ C++ HackerRank/variable-sized-arrays.cpp
@@ -0,0 +1,22 @@
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+int main()
+{
+    int s, q, element;
+    int **innerArray;
+    cin >> s >> q;
+    int *mainArray;
+    //mainArray = new int[element];
+    for (int i = 0; i < s; ++i)
+    {
+        cin >> element;
+        mainArray = new int[0][element];
+        for (int j = 0; j < element; j++)
+        {
+
+        }
+    }
+}
