2dcb470bb9198c6b2c0e6074efd99fcc9766c3cd
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-19 23:28:05 +0530

    time limit exceeded

--- CSES/BeutifulPermutations.cpp
+++ CSES/BeutifulPermutations.cpp
@@ -9,12 +9,28 @@ int main(){
     int n;
     cin >>n;
     int a[n];
-    int a,b;
-    cin >> a;
-    for(int i=1; i<=n; i++){
-        a[i] = i;
+    for(int i=0; i<n; i++){
+        a[i] = i+1;
     }
-    do {  
-    cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';  
-    } while ( next_permutation(myints,myints+3) ); 
+    bool is = false;
+    do {
+        int first = a[0];
+        is = false;
+        string s = to_string(first);
+         for (int i = 1; i < n; i++) {
+             if(abs(first-a[i]) <= 1){
+                 is = true;
+                 break;
+             }
+             s = s +" "+to_string(a[i]);
+             first = a[i];
+        }
+        if(!is){
+            cout << s; 
+            break;
+        }
+    } while ( next_permutation(a,a+n) ); 
+    if(is){
+        cout<<"NO SOLUTION";
+    }
 }
