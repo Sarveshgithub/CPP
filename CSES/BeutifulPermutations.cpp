85a5b7480d02c0d203db099abec65de2b0fe38e4
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-20 01:31:42 +0530

    Add Proper Solution

--- CSES/BeutifulPermutations.cpp
+++ CSES/BeutifulPermutations.cpp
@@ -1,36 +1,18 @@
-//Author : Sarvesh
-#include <bits/stdc++.h>
-typedef long long ll;
+// https://cses.fi/problemset/task/1070/
+#include <iostream>
+
 using namespace std;
-#define f(x, n) for (int x = 0; x < n; ++x)
-#define f1(x, n) for (int x = 1; x <= n; ++x)
-#define ed endl;
-int main(){
-    int n;
-    cin >>n;
-    int a[n];
-    for(int i=0; i<n; i++){
-        a[i] = i+1;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n == 1) cout << "1\n";
+  else if (n < 4) cout << "NO SOLUTION\n";
+  else if (n == 4) cout << "3 1 4 2\n";
+  else {
+    for (int i = 1; i <= n; i += 2) cout << i << " ";
+    for (int i = 2; i <= n; i += 2) {
+      cout << i<<" ";
     }
-    bool is = false;
-    do {
-        int first = a[0];
-        is = false;
-        string s = to_string(first);
-         for (int i = 1; i < n; i++) {
-             if(abs(first-a[i]) <= 1){
-                 is = true;
-                 break;
-             }
-             s = s +" "+to_string(a[i]);
-             first = a[i];
-        }
-        if(!is){
-            cout << s; 
-            break;
-        }
-    } while ( next_permutation(a,a+n) ); 
-    if(is){
-        cout<<"NO SOLUTION";
-    }
+  }
 }
