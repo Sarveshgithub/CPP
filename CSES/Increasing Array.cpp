32545fe198b9130451ed6c42a26a43d2642bc296
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-21 00:54:57 +0530

    format code

--- CSES/Increasing Array.cpp
+++ CSES/Increasing Array.cpp
@@ -2,21 +2,20 @@
 #include <vector>
 using namespace std;
 
-int main(){
-    int n,a,b;
-    long long turn=0;
-    cin>>n;
-    cin>>a;
-    for(int i=1;i<n;i++){
+int main() {
+    int n, a, b;
+    long long turn = 0;
+    cin >> n;
+    cin >> a;
+    for (int i = 1; i < n; i++) {
         cin >> b;
-        if(a>b){
-            turn += a-b;
-        }
-        else{
+        if (a > b) {
+            turn += a - b;
+        } else {
             a = b;
         }
     }
-    cout<<turn<<"\n";
+    cout << turn << "\n";
 
     return 0;
 }
--- CSES/Number Spiral.cpp
+++ CSES/Number Spiral.cpp
@@ -4,42 +4,27 @@ using namespace std;
 #define f(x, n) for (int x = 0; x < n; ++x)
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
-int main()
-{
+int main() {
     long long n;
     cin >> n;
-    while (n > 0)
-    {
+    while (n > 0) {
         long long x, y;
         cin >> x >> y;
-        if (x > y)
-        {
-            if (x % 2 == 0)
-            {
+        if (x > y) {
+            if (x % 2 == 0) {
                 cout << x * x - (x - 1) + (x - y);
-            }
-            else
-            {
+            } else {
                 cout << x * x - (x - 1) - (x - y);
             }
-        }
-        else if (x < y)
-        {
-            if (y % 2 != 0)
-            {
+        } else if (x < y) {
+            if (y % 2 != 0) {
                 cout << y * y - (y - 1) + (y - x);
-            }
-            else
-            {
+            } else {
                 cout << y * y - (y - 1) - (y - x);
             }
-        }
-        else if (x == 1 && y == 1)
-        {
+        } else if (x == 1 && y == 1) {
             cout << 1;
-        }
-        else
-        {
+        } else {
             cout << (x * x) - (x - 1);
         }
         cout << "\n";
