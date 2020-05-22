fecdcb37d6cc652c5e46d9d2c26f48be9c896de5
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-22 16:30:18 +0530

    format the code

--- template.cpp
+++ template.cpp
@@ -1,10 +1,11 @@
 //Author : Sarvesh
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 #define f(x, n) for (int x = 0; x < n; ++x)
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
-int main(){
-    string s="dfdf";
-    cout<<(s.find("d") != string::npos);
+int main() {
+    string s = "dfdf";
+    cout << (s.find("d") != string::npos);
 }
