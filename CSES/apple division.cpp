d80d1a30561fd9da881039d4f249d7ec0866981c
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-05-27 22:34:28 +0530

    apple division

--- CSES/apple division.cpp
+++ CSES/apple division.cpp
@@ -5,6 +5,38 @@ typedef long long ll;
 #define f(x, n) for (int x = 0; x < n; ++x)
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
+vector<int> inputArray;
+vector<int> subset;
+ll n, m, t;
+int a = 0, b = 0;
+void search(int k) {
+    if (k == n) {
+        int s = 0;
+        f(i, subset.size()) {
+            s += subset[i];
+            // cout << subset[i] << " ";
+        }
+        ll x = s;
+        ll y = t - s;
+        if (y > x) swap(x, y);
+        m = min(x - y, m);
+    } else {
+        subset.push_back(inputArray[k]);
+        search(k + 1);
+        subset.pop_back();
+        search(k + 1);
+    }
+}
 int main() {
-    
+    cin >> n;
+    a = n / 2;
+    b = n - a;
+    f(i, n) {
+        int j;
+        cin >> j;
+        inputArray.push_back(j);
+        t += j;
+    }
+    search(0);
+    cout << "m:::" << m;
 }
