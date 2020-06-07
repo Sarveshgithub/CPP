e795010f53b4901b1e066f5a0c97bec54c594b77
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-06-07 23:05:23 +0530

    Appartment

--- CSES/Apartments.cpp
+++ CSES/Apartments.cpp
@@ -1,11 +1,43 @@
-//Author : Sarvesh
 #include <bits/stdc++.h>
+#define lli long long int
+#define li long int
+#define ld long double
 using namespace std;
-typedef long long ll;
-#define f(x, n) for (ll x = 0; x < n; ++x)
-#define f1(x, n) for (ll x = 1; x <= n; ++x)
-#define ed endl;
-ll n, m, k;
-int main() {
-    
+const lli mod = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> peeps(n);
+    vector<int> apps(m);
+    for (int i = 0; i < n; i++)
+        cin >> peeps[i];
+    for (int i = 0; i < m; i++)
+        cin >> apps[i];
+    sort(peeps.begin(), peeps.end());
+    sort(apps.begin(), apps.end());
+    int ans = 0;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (abs(peeps[i] - apps[j]) <= k)
+        {
+            ans++;
+            i++;
+            j++;
+        }
+        else if (peeps[i] < apps[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    cout << ans;
+    return 0;
 }
