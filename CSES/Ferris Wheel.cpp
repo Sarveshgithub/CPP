d0d73dc925dba4c48659bbb3fda23e3233b44c52
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-06-08 23:00:02 +0530

    ferris wheel

---
+++ CSES/Ferris Wheel.cpp
@@ -0,0 +1,31 @@
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+#define ed endl;
+int main() {
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    f(i, n) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int c = 0, s = 0;
+    for (int i = 1; i <= n; i= i+2) {
+        if (a[i - 1] + a[i] <= x) {
+            c += 1;
+        } else {
+            c += 2;
+        }
+    }
+    cout << c;
+}
+
+//9 8 8 9 10 8 5 8 7 10
+//5 7 8 8 8 8 9 9 10 10 
+
+15
+1 
