1edaaa33e3d1c5e74725548217132d7fa3d3bc7b
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-06-18 23:18:36 +0530

    ferris wheel

--- CSES/Ferris Wheel.cpp
+++ CSES/Ferris Wheel.cpp
@@ -6,31 +6,22 @@ typedef long long ll;
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
 int main() {
-    ll n, x;
+    int n, x;
     cin >> n >> x;
-    vector<ll> a(n);
+    int  a[n];
     f(i, n) {
         cin >> a[i];
     }
-    sort(a.begin(), a.end());
-    int c = 0, s = 0;
-    int size = a.size() - 1;
-    while ((size) >= 0) {
-        if (a[size] <= x) {
-            int rem = x - a[size];
-            for (int i = 0; i < size - 1; i++) {
-                cout << ":::::" << a[i];
-                if (a[i] <= rem) {
-                    a.erase(a.begin() + i);
-                    break;
-                }
-            }
-        }
-        a.erase(a.begin() + size);
-        c += 1;
+    sort(a,a+n);
+    int c = 0;
+    for (int i = 0, j = n - 1; i < j;) {
+        while (i < j && a[i] + a[j] > x)
+            --j;
+        if (i >= j)
+            break;
+        ++c;
+        ++i;
+        --j;
     }
-    cout << c;
+    cout << n-c;
 }
-
-//9 8 8 9 10 8 5 8 7 10
-//5 7 8  9  10
