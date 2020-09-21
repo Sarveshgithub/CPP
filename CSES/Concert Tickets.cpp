6f4a59d66a10af7e9e5cfd28b6ca26aaf05ea9f2
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-09-21 22:46:23 +0530

    update cls

--- CSES/Concert Tickets.cpp
+++ CSES/Concert Tickets.cpp
@@ -6,5 +6,14 @@ typedef long long ll;
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
 int main() {
-    cout << "sarvesh kumar";
+    map<ll, ll> tickets;
+    tickets[1] = 1;
+    ll m,n;
+    cin >> m >>n;
+    f(i,m){
+        
+    }
+    for (map<ll, ll>::iterator ii = tickets.begin(); ii != tickets.end(); ++ii) {
+        cout << (*ii).first << ": " << (*ii).second << endl;
+    }
 }
