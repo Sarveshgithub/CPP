b3e1bac08b9f36dae1590200aef8bfb2b6931b98
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2021-05-20 22:33:21 +0530

    new problem

---
+++ CSES/gray code.cpp
@@ -0,0 +1,12 @@
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+#define ed endl;
+int main() {
+    ll n;
+    cin >>n;
+    cout << pow(2,n);
+}
