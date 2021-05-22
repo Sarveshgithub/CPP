86bf5c18200702a5fbccf2cc298b029a4a09911e
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2021-05-22 22:26:24 +0530

    add problem

--- CSES/gray code.cpp
+++ CSES/gray code.cpp
@@ -5,8 +5,29 @@ typedef long long ll;
 #define f(x, n) for (int x = 0; x < n; ++x)
 #define f1(x, n) for (int x = 1; x <= n; ++x)
 #define ed endl;
+void GreyCode(int n) {
+    // power of 2
+    for (int i = 0; i < (1 << n); i++) {
+        // Generating the decimal
+        // values of gray code then using
+        // bitset to convert them to binary form
+        int val = (i ^ (i >> 1));
+
+        // Using bitset
+        bitset<32> r(val);
+
+        // Converting to string
+        string s = r.to_string();
+        cout << s.substr(32 - n) << ed;
+    }
+}
+
+// Driver Code
 int main() {
-    ll n;
-    cin >>n;
-    cout << pow(2,n);
+    int n;
+    cin >> n;
+    // Function call
+    GreyCode(n);
+
+    return 0;
 }
