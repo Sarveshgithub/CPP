dcfe7458e7a936de7b936f3612e5b8234a4b72f6
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2021-06-30 16:01:56 +0530

    pretty permutation code apex

---
+++ CP/retty Permutations.cpp
@@ -0,0 +1,24 @@
+//Author : Sarvesh
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(x, n) for (int x = 0; x < n; ++x)
+#define f1(x, n) for (int x = 1; x <= n; ++x)
+#define ed endl;
+int main() {
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+    #endif
+	int n;
+	cin >> n;
+	while(n--){
+		int t;
+		cin >>t;
+		cout<< t << " ";
+		f1(x,t-1){
+			cout << x << " ";
+		}
+		cout << ed;
+	}
+}
